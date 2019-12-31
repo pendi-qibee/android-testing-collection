a0ff07df9d44660ecdb8e98c5573e8e550e81be6
pendi-qibee <pendimadyana@gmail.com>
2020-01-01 06:54:27 +0700

    set button Pesan to show respons

--- app/src/main/java/id/qibee/androidtestingcollection/EditTextTestingActivity.kt
+++ app/src/main/java/id/qibee/androidtestingcollection/EditTextTestingActivity.kt
@@ -1,12 +1,26 @@
 package id.qibee.androidtestingcollection
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_edit_text_testing.*
 
 class EditTextTestingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_text_testing)
+
+        btnPesan.setOnClickListener {
+
+            //menyimpan value dari edittext
+            val menu1 = etMenu1.text.toString()
+            val menu2 = etMenu2.text.toString()
+
+            //menampilkan textview respon pesanan
+            tvResponPesanan.visibility = View.VISIBLE
+            tvResponPesanan.text = "Pesanan kamu sudah dicatat : \n $menu1 dan $menu2"
+        }
     }
 }
