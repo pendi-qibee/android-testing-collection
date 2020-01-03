0b42a899184cb27093ac21f19782fe9a717d5df4
pendi-qibee <pendimadyana@gmail.com>
2020-01-03 13:37:55 +0700

    record espresso test for TimePicker

---
+++ app/src/androidTest/java/id/qibee/androidtestingcollection/TimePickerTestFromRecording.kt
@@ -0,0 +1,241 @@
+package id.qibee.androidtestingcollection
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class TimePickerTestFromRecording {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun timePickerTestFromRecording() {
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.tvTimePickerTesting), withText("Tes Time Picker"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val imageButton = onView(
+            allOf(
+                withId(R.id.btnSetTime),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        imageButton.check(matches(isDisplayed()))
+
+        val appCompatImageButton = onView(
+            allOf(
+                withId(R.id.btnSetTime),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+
+        val numericTextView = onView(
+            allOf(
+                withClassName(`is`("com.android.internal.widget.NumericTextView")), withText("09"),
+                childAtPosition(
+                    allOf(
+                        withClassName(`is`("android.widget.RelativeLayout")),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        numericTextView.perform(click())
+
+        val numericTextView2 = onView(
+            allOf(
+                withClassName(`is`("com.android.internal.widget.NumericTextView")), withText("09"),
+                childAtPosition(
+                    allOf(
+                        withClassName(`is`("android.widget.RelativeLayout")),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        numericTextView2.perform(click())
+
+        val numericTextView3 = onView(
+            allOf(
+                withClassName(`is`("com.android.internal.widget.NumericTextView")), withText("15"),
+                childAtPosition(
+                    allOf(
+                        withClassName(`is`("android.widget.RelativeLayout")),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        numericTextView3.perform(click())
+
+        val numericTextView4 = onView(
+            allOf(
+                withClassName(`is`("com.android.internal.widget.NumericTextView")), withText("15"),
+                childAtPosition(
+                    allOf(
+                        withClassName(`is`("android.widget.RelativeLayout")),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        numericTextView4.perform(click())
+
+        val appCompatButton2 = onView(
+            allOf(
+                withId(android.R.id.button1), withText("OK"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        appCompatButton2.perform(scrollTo(), click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.tvTime), withText("13:15"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("13:15")))
+
+        val appCompatImageButton2 = onView(
+            allOf(
+                withId(R.id.btnSetDate),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton2.perform(click())
+
+        val appCompatButton3 = onView(
+            allOf(
+                withId(android.R.id.button1), withText("OK"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        appCompatButton3.perform(scrollTo(), click())
+
+        val textView2 = onView(
+            allOf(
+                withId(R.id.tvDate), withText("2020-1-4"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("2020-1-4")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
