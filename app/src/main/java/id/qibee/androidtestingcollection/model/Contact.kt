53f1ccc0ac4a327fdee5f31d16458d809213ec76
pendi-qibee <pendimadyana@gmail.com>
2020-01-17 17:46:45 +0700

    implement Parcelable

--- app/src/main/java/id/qibee/androidtestingcollection/model/Contact.kt
+++ app/src/main/java/id/qibee/androidtestingcollection/model/Contact.kt
@@ -1,7 +1,11 @@
 package id.qibee.androidtestingcollection.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Contact(
     val id: Int,
     val aplikasi: String,
     val akun: String
-)
+) : Parcelable
