628a988773a9fe7f6a7e4700fdbc6d458b584505
pendi-qibee <pendimadyana@gmail.com>
2020-01-26 23:54:05 +0700

    add Recyclerview test from recording

---
+++ app/src/androidTest/java/id/qibee/androidtestingcollection/RecyclerviewTestRecording.kt
@@ -0,0 +1,132 @@
+package id.qibee.androidtestingcollection
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class RecyclerviewTestRecording {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun recyclerviewTestRecording() {
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.btnRecyclerViewTesting), withText("Tes Recyclerview"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val constraintLayout = onView(
+            allOf(
+                childAtPosition(
+                    allOf(
+                        withId(R.id.rvContact),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        constraintLayout.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.tvNamaAkunDetail), withText("pendi-qibee"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("pendi-qibee")))
+
+        pressBack()
+
+        val constraintLayout2 = onView(
+            allOf(
+                childAtPosition(
+                    allOf(
+                        withId(R.id.rvContact),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        constraintLayout2.perform(click())
+
+        val textView2 = onView(
+            allOf(
+                withId(R.id.tvNamaAkunDetail), withText("@pendimadyana"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("@pendimadyana")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
